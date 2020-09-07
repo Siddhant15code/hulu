022b86855ffdd7ba10b6602d8015f05085c5a4a4
Siddhant <mamtajain81@gmail.com>
2020-09-07 11:01:25 +0530

    Updates

---
+++ precache-manifest.e85ad6ca9686a5ca01a03110b9009890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd02cffb0e0dd50ff969fd05232ae0b",
+    "url": "/hulu/index.html"
+  },
+  {
+    "revision": "94199874c81577dd250c",
+    "url": "/hulu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b626e94efd7de75d99d",
+    "url": "/hulu/static/js/2.be0a9907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hulu/static/js/2.be0a9907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94199874c81577dd250c",
+    "url": "/hulu/static/js/main.412c261b.chunk.js"
+  },
+  {
+    "revision": "c69d03e855e312676a70",
+    "url": "/hulu/static/js/runtime-main.ad6c0845.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hulu/static/media/logo.5d5d9eef.svg"
+  }
+]);
